576054e63783c1cef08ad2c23b96ab8b529b73c0
Sam Schoenberg <sam4130@Sams-MacBook-Pro.local>
2020-05-27 19:49:45 -0600

    added blog files

--- src/index.js
+++ src/index.js
@@ -2,16 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// global url
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+// token and content type
+//axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+//axios.defaults.headers.post['Content-Type'] = 'application/jason';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// how to configure global access and error handling with servers
+axios.interceptors.request.use(request =>{
+        console.log(request);
+        // must always return the request in order not to block it
+        return request;
+
+    },// this error will only show up if their is no internet connection or somthing like that
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    });
+
+axios.interceptors.response.use( response =>{
+        console.log(response);
+        // must always return the request in order not to block it
+        return response;
+
+    },// this error will only show up if their is no internet connection or somthing like that
+    error => {
+        console.log(error);
+        return Promise.reject(error);
+    });
+
+
+ReactDOM.render( <App />, document.getElementById( 'root' ) );
+registerServiceWorker();
